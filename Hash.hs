f0af09f1443349631981de7a669633f683b5a62e
dkarlo2 <dkarlo2@gmail.com>
2015-01-06 20:24:17 +0100

    V0.1

---
+++ Hash.hs
@@ -0,0 +1,49 @@
+-- The top-level module. Connects parsing to execution and adds interaction
+-- with the user / reading from file.
+
+module Hash (runScript, runInteractive) where
+
+import Commands
+import Exec
+import HashParser
+import System.Console.Haskeline
+import System.Directory
+
+-- Runs a .hash script
+runScript :: FilePath -> IO ()
+runScript fp = do
+  wd <- getCurrentDirectory
+  script <- readFile fp
+  case parseProgram (removeComments script ++ ";") of
+       Left err -> do
+         putStrLn $ "!!! Parse error: " ++ err
+       Right xs -> do
+         runHashProgram commands (Left wd) xs
+         return ()
+
+-- Communicates with the user and performs hash commands line by line.
+runInteractive :: IO ()
+runInteractive = do
+  wd <- getCurrentDirectory
+  runInteractive' (Left wd)
+    where runInteractive' :: Either FilePath ScriptState -> IO ()
+          runInteractive' fpss = do
+            line <- case fpss of
+                         Left wd -> getNextLine wd
+                         Right (ScriptState _ wd _) -> getNextLine wd
+            case parseProgram line of
+              Left err -> do
+                putStrLn $ "!!! Parse error: " ++ err
+                runInteractive' fpss
+              Right xs -> do
+                state <- runHashProgram commands fpss xs
+                runInteractive' (Right state)
+
+-- Gets next line from user input.
+getNextLine :: FilePath -> IO String
+getNextLine wd = do
+  line <- runInputT defaultSettings $ getInputLine $ wd ++ "% "
+  case line of
+       Just l -> return $ (removeComment l) ++ ";"
+       Nothing -> error "Error on user input"
+
